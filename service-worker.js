efca10f87f673be02494fca86b2fa3203f11bcce
kay-jp <kay77g@gmail.com>
2025-10-16 14:29:25 +0900

    adsa

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,28 @@
+
+
+var CACHE_NAME = 'task-management';
+var urlsToCache = [
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
 // Listen to the widgetinstall event.
 self.addEventListener("widgetinstall", event => {
   // The widget just got installed, render it using renderWidget.
