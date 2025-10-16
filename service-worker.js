c760023fb4a3cfce7805cec5d83fa3f807087b23
kay-jp <kay77g@gmail.com>
2025-10-16 14:20:17 +0900

    d

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,53 @@
-self.addEventListener('notificationclick', function (event) {
-    event.notification.close();
-    event.waitUntil(
-        clients.openWindow('https://example.com')
-    );
+self.addEventListener("widgetinstall", event => {
+  event.waitUntil(onWidgetInstall(event.widget));
 });
+
+self.addEventListener("widgetuninstall", event => {
+  event.waitUntil(onWidgetUninstall(event.widget));
+});
+
+async function onWidgetInstall(widget) {
+  // Register a periodic sync, if this wasn't done already.
+  // We use the same tag for the sync registration and the widget to
+  // avoid registering several periodic syncs for the same widget.
+  const tags = await self.registration.periodicSync.getTags();
+  if (!tags.includes(widget.definition.tag)) {
+    await self.registration.periodicSync.register(widget.definition.tag, {
+      minInterval: widget.definition.update
+    });
+  }
+
+  // And also update the instance.
+  await updateWidget(widget);
+}
+
+async function onWidgetUninstall(widget) {
+  // On uninstall, unregister the periodic sync.
+  // If this was the last widget instance, then unregister the periodic sync.
+  if (widget.instances.length === 1 && "update" in widget.definition) {
+    await self.registration.periodicSync.unregister(widget.definition.tag);
+  }
+}
+
+// Listen to periodicsync events to update all widget instances
+// periodically.
+self.addEventListener("periodicsync", async event => {
+  const widget = await self.widgets.getByTag(event.tag);
+
+  if (widget && "update" in widget.definition) {
+    event.waitUntil(updateWidget(widget));
+  }
+});
+
+async function updateWidget(widget) {
+  // Get the template and data URLs from the widget definition.
+  const templateUrl = widget.definition.msAcTemplate;
+  const dataUrl = widget.definition.data;
+
+  // Fetch the template text and data.
+  const template = await (await fetch(templateUrl)).text();
+  const data = await (await fetch(dataUrl)).text();
+
+  // Render the widget with the template and data.
+  await self.widgets.updateByTag(widget.definition.tag, {template, data});
+}
