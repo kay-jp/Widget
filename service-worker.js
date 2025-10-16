2174360e75ace7c971fe21c98ee0fbd0a78e2a9b
kay-jp <kay77g@gmail.com>
2025-10-16 11:16:21 +0900

    a

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('notificationclick', function (event) {
     event.notification.close();
     event.waitUntil(
-        clients.openWindow('https://example.com')
+        clients.openWindow('https://9f476953-51e0-489c-9a0d-1242faf36c2c-00-3mbiv4ssl2rk7.pike.replit.dev/')
     );
 });
