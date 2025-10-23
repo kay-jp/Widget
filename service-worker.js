eb1d2a4e3738de35f24207108610cc210a53b334
kay-jp <kay77g@gmail.com>
2025-10-23 10:47:22 +0900

    d

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,57 @@
-
-
-var CACHE_NAME = 'pwatest';
-var urlsToCache = [
-];
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response || fetch(event.request);
-            })
-    );
-});
 // Listen to the widgetinstall event.
 self.addEventListener("widgetinstall", event => {
-  // The widget just got installed, render it using renderWidget.
-  // Pass the event.widget object to the function.
-  event.waitUntil(renderWidget(event.widget));
+  event.waitUntil(onWidgetInstall(event.widget));
 });
+
+self.addEventListener("widgetuninstall", event => {
+  event.waitUntil(onWidgetUninstall(event.widget));
+});
+
+async function onWidgetInstall(widget) {
+  // Register a periodic sync, if this wasn't done already.
+  // We use the same tag for the sync registration and the widget to
+  // avoid registering several periodic syncs for the same widget.
+  const tags = await self.registration.periodicSync.getTags();
+  if (!tags.includes(widget.definition.tag)) {
+    await self.registration.periodicSync.register(widget.definition.tag, {
+      minInterval: widget.definition.update
+    });
+  }
+
+  // And also update the instance.
+  await updateWidget(widget);
+}
+
+async function onWidgetUninstall(widget) {
+  // On uninstall, unregister the periodic sync.
+  // If this was the last widget instance, then unregister the periodic sync.
+  if (widget.instances.length === 1 && "update" in widget.definition) {
+    await self.registration.periodicSync.unregister(widget.definition.tag);
+  }
+}
+
+// Listen to periodicsync events to update all widget instances
+// periodically.
+self.addEventListener("periodicsync", async event => {
+  const widget = await self.widgets.getByTag(event.tag);
+
+  if (widget && "update" in widget.definition) {
+    event.waitUntil(updateWidget(widget));
+  }
+});
+
+async function updateWidget(widget) {
+  // Get the template and data URLs from the widget definition.
+  const templateUrl = widget.definition.msAcTemplate;
+  const dataUrl = widget.definition.data;
+
+  // Fetch the template text and data.
+  const template = await (await fetch(templateUrl)).text();
+  const data = await (await fetch(dataUrl)).text();
+
+  // Render the widget with the template and data.
+  await self.widgets.updateByTag(widget.definition.tag, {template, data});
+}
 
 async function renderWidget(widget) {
   // Get the template and data URLs from the widget definition.
@@ -42,24 +65,13 @@ async function renderWidget(widget) {
   // Render the widget with the template and data.
   await self.widgets.updateByTag(widget.definition.tag, {template, data});
 }
-
-// Update the widgets to their initial states
-// when the service worker is activated.
-self.addEventListener("activate", event => {
-  event.waitUntil(updateWidgets());
-});
-
-async function updateWidgets() {
-  // Get the widget that match the tag defined in the web app manifest.
-  const widget = await self.widgets.getByTag("pwatest");
-  if (!widget) {
-    return;
+self.addEventListener('widgetclick', (event) => {
+  switch (event.action) {
+    case 'previous-song':
+      // Application logic to play the previous song...
+      break;
+    case 'next-song':
+      // Application logic to play the next song...
+      break;
   }
-
-  // Using the widget definition, get the template and data.
-  const template = await (await fetch(widget.definition.msAcTemplate)).text();
-  const data = await (await fetch(widget.definition.data)).text();
-
-  // Render the widget with the template and data.
-  await self.widgets.updateByTag(widget.definition.tag, {template, data});
-}
+});
